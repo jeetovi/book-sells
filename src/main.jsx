46f85cfb2b333220df85842563c1b44fa7324c6b
AVIJIT BISWAS <112837308+AVIJIT10-ops@users.noreply.github.com>
2023-04-08 00:50:15 +0600

    all link setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element:   <App/>,
+  },
+  {
+    path: '/about',
+    element: <p>About Page</p>,
+
+
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<RouterProvider router ={router}/>
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
 )
